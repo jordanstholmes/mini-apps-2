aaa86dec2251796e0f2d05bf6d3f5c6fb4db71ff
Jordan Holmes <jordanstholmes@gmail.com>
2018-12-05 12:02:14 -0800

    Setup webpack with basic test react component

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,7 @@
+/* eslint-env browser */
+import React from 'react';
+import ReactDom from 'react-dom';
 
+const App = () => <h1>this is from React</h1>;
 
+ReactDom.render(<App />, document.getElementById('app'));
