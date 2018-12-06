7a68e766df560b47d471d66277fcfdf00f3d5476
Jordan Holmes <jordanstholmes@gmail.com>
2018-12-05 16:40:17 -0800

    Connect fetch and pagination functionality

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -3,41 +3,86 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import styled from 'styled-components';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
+import PropTypes from 'prop-types';
 import EventList from './components/eventList';
 
 import Search from './components/search';
 
 const apiUrl = process.env.API_URL || 'http://localhost:3000';
 
-const Wrapper = styled.section`
+const ColumnWrapper = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
+const RowWrapper = styled.div`
+  display: flex;
+`;
+
 class App extends React.Component {
+  static fetchEvents(page, perPage) {
+    return axios.get(`${apiUrl}/events?_page=${page}&_limit=${perPage}`)
+      .catch(err => console.error(err));
+  }
+
   constructor(props) {
     super(props);
     this.state = {
       events: [],
+      page: 0,
+      pageCount: 0,
     };
+    this.handlePageClick = this.handlePageClick.bind(this);
   }
 
   componentDidMount() {
-    axios.get(`${apiUrl}/events?_page=0`)
-      .then(res => this.setState({ events: res.data }))
-      .catch(err => console.error(err));
+    const { page } = this.state;
+    const { perPage } = this.props;
+    App.fetchEvents(page, perPage)
+      .then(res => this.setState({
+        events: res.data,
+        pageCount: Number(res.headers['x-total-count']) / perPage,
+      }));
+  }
+
+  handlePageClick({ selected }) {
+    const { perPage } = this.props;
+    App.fetchEvents(selected, perPage)
+      .then(res => this.setState({ events: res.data, pageCount: Number(res.headers['x-total-count']) }));
   }
 
   render() {
-    const { events } = this.state;
+    const { events, pageCount } = this.state;
     return (
-      <Wrapper>
+      <ColumnWrapper>
         <Search />
         {events.length === 0 ? '' : <EventList events={events} />}
-      </Wrapper>
+
+        <RowWrapper>
+          {
+            pageCount && (
+              <ReactPaginate
+                previousLabel="previous"
+                nextLabel="next"
+                breakLabel="..."
+                breakClassName="break-me"
+                pageCount={pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={this.handlePageClick}
+              />
+            )
+          }
+        </RowWrapper>
+      </ColumnWrapper>
     );
   }
 }
 
-ReactDom.render(<App />, document.getElementById('app'));
+App.propTypes = {
+  perPage: PropTypes.number.isRequired,
+};
+
+ReactDom.render(<App perPage={10} />, document.getElementById('app'));
