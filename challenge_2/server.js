1f51cda068ef814597e4c2a05140c2c6d3a968c9
Jordan Holmes <jordanstholmes@gmail.com>
2018-12-06 14:41:43 -0800

    Add basic server and dev start script

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+const app = express();
+const logging = process.env.NODE_ENV === 'production' ? morgan('short') : morgan('dev');
+const PORT = process.env.PORT || 3000;
+
+app.use(logging);
+app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.listen(3000);
+console.log(`Express listening at port ${PORT}`);
 
