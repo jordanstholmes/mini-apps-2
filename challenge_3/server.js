3007edcc1edb2dd557489840debda035feb61df0
Jordan Holmes <jordanstholmes@gmail.com>
2018-12-07 11:25:43 -0800

    Complete initial setup

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 
+const app = express();
+const logging = process.env.NODE_ENV === 'production' ? morgan('short') : morgan('dev');
+const PORT = process.env.PORT || 3000;
+
+app.use(logging);
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.listen(PORT);
+console.log(`Express listening at ${PORT}`);
 
