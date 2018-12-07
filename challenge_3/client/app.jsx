29046e752d77db7e1987409096233da268ca2311
Jordan Holmes <jordanstholmes@gmail.com>
2018-12-07 15:28:28 -0800

    Change client app.jsx to test new component renders

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,9 +1,10 @@
 /* eslint-env browser */
 import React from 'react';
 import ReactDom from 'react-dom';
+import FrameShots from './components/frameShots';
 
 const App = ({ name }) => <h1 className={name}>Hello from React!</h1>;
 
-// ReactDom.render(<App />, document.getElementById('app'));
+ReactDom.render(<FrameShots first={5} second={0} />, document.getElementById('app'));
 
 export default App;
