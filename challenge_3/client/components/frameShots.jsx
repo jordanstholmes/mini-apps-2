92f6f9e8e6df1a7a3b1f63618bc0fd8e61553bb8
Jordan Holmes <jordanstholmes@gmail.com>
2018-12-07 16:24:01 -0800

    Fix logic in FrameShot component for spares

--- challenge_3/client/components/frameShots.jsx
+++ challenge_3/client/components/frameShots.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const convertToDisplay = (shot) => {
+const convertShot = (shot) => {
   let display;
   if (shot === 0) {
     display = '-';
@@ -15,16 +15,26 @@ const convertToDisplay = (shot) => {
   return display;
 };
 
-const FrameShots = ({ first, second }) => (
-  <table>
-    <tbody>
-      <tr>
-        <td>{convertToDisplay(first)}</td>
-        <td>{convertToDisplay(second)}</td>
-      </tr>
-    </tbody>
-  </table>
-);
+const convertShotsToDisplay = (shot1, shot2) => {
+  if (shot1 + shot2 === 10) {
+    return [convertShot(shot1), '/'];
+  }
+  return [convertShot(shot1), convertShot(shot2)];
+};
+
+const FrameShots = ({ first, second }) => {
+  const shotsForDisplay = convertShotsToDisplay(first, second);
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>{shotsForDisplay[0]}</td>
+          <td>{shotsForDisplay[1]}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
 FrameShots.propTypes = {
   first: PropTypes.number.isRequired,
